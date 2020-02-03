2efb8210010995f2cf6228de83bd9daa32407938
Mihai Smarandache <mihai.sanfran@gmail.com>
2020-02-02 18:58:18 -0700

    Register service worker

---
+++ public/src/js/app.js
@@ -0,0 +1,15 @@
+// Register service worker
+// navigator = browser
+
+// Detect if service worker feature is available in the browser
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
+      }, function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+      });
+    });
+}
