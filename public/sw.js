7e6518d8dc68b320112c09c925a3220f71951767
Mihai Smarandache <mihai.sanfran@gmail.com>
2020-02-07 12:05:00 -0700

    Add service worker fetch event

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,18 @@
+/* Lifecycle Events */
 self.addEventListener('install', function(event){
-    console.log('Installing service worker...')
+    console.log('[Service Worker] Installing service worker...', event)
 });
 
 self.addEventListener('activate', function(event){
-    console.log('Activating service worker...')
+    console.log('[Service Worker] Activating service worker...', event)
 
-    return self.ClientRectList.claim();
+    return self.clients.claim();
+});
+
+/* Non-lifecycle events */
+// Triggered when loading images, css files, async etc (Network proxy)
+self.addEventListener('fetch', function(event){
+    console.log('[Service Worker] Fetching something...', event)
+
+    event.respondWith(fetch(event.request));
 });
