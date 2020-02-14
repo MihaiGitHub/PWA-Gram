fc445f95d45d1e4ee0e1ae633ae94490eb726f30
Mihai Smarandache <mihai.sanfran@gmail.com>
2020-02-14 10:19:05 -0700

    Project cleanup

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,34 +1,24 @@
-// Register service worker
-// navigator = browser
 
 var deferredPrompt;
 
-if(!window.Promise){
+if (!window.Promise) {
   window.Promise = Promise;
 }
 
-// Detect if service worker feature is available in the browser
 if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function() {
-      navigator.serviceWorker.register('/sw.js').then(function(registration) {
-        // Registration was successful
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      }, function(err) {
-        // registration failed :(
-        console.log('ServiceWorker registration failed: ', err);
-      });
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function () {
+      console.log('Service worker registered!');
+    })
+    .catch(function(err) {
+      console.log(err);
     });
 }
 
-// Triggered right before about to show Install banner
-window.addEventListener('beforeinstallprompt', function(event){
-  console.log('beforeinstallprompt fired')
-
-  // Prevent install banner
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
   event.preventDefault();
-
-  // Save event to trigger later
   deferredPrompt = event;
-
   return false;
 });
--- public/sw.js
+++ public/sw.js
@@ -1,19 +1,13 @@
-/* Lifecycle Events */
-self.addEventListener('install', function(event){
-    console.log('[Service Worker] Installing service worker...', event)
+
+self.addEventListener('install', function(event) {
+  console.log('[Service Worker] Installing Service Worker ...', event);
 });
 
-self.addEventListener('activate', function(event){
-    console.log('[Service Worker] Activating service worker...', event)
-
-    return self.clients.claim();
+self.addEventListener('activate', function(event) {
+  console.log('[Service Worker] Activating Service Worker ....', event);
+  return self.clients.claim();
 });
 
-/* Non-lifecycle events */
-// Triggered when loading images, css files, async etc (Network proxy)
-self.addEventListener('fetch', function(event){
-    console.log('[Service Worker] Fetching something...', event)
-
-    event.respondWith(fetch(event.request));
+self.addEventListener('fetch', function(event) {
+  event.respondWith(fetch(event.request));
 });
-
