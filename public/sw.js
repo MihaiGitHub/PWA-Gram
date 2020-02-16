156fbc11d56df4fd495d39887606c81084edb06d
Mihai Smarandache <mihai.sanfran@gmail.com>
2020-02-16 11:03:05 -0700

    Cache multiple files with addAll

--- public/sw.js
+++ public/sw.js
@@ -1,17 +1,30 @@
-
+// when the browser sees this SW for the first time
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
 
+  // wait until the following promise resolves
   // Precaching using Cache API - store static files on service worker installation
   event.waitUntil(
-    // Returns a promise
+    // open/create a cache named 'static'
     caches.open('static').then(function(cache) {
-      console.log('[Service Worker] Precaching App Shell')
-
-      // Add files to the cache
-      cache.add('/src/js/app.js')
+      // Add "requests" to the cache; If any fail it will cause the addAll() to reject
+      return cache.addAll([
+    //    '/',
+        '/index.html',
+        '/src/js/app.js',
+        '/src/js/feed.js',
+        '/src/js/promise.js',
+        '/src/js/fetch.js',
+        '/src/js/material.min.js',
+        '/src/css/app.css',
+        '/src/css/feed.css',
+        '/src/images/main-image.jpg',
+        'https://fonts.googleapis.com/css?family=Roboto:400,700',
+        'https://fonts.googleapis.com/icon?family=Material+Icons',
+        'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+      ]);
     })
-  )
+  );
 });
 
 self.addEventListener('activate', function(event) {
@@ -33,7 +46,6 @@ self.addEventListener('fetch', function(event) {
         return fetch(event.request);
       }
     })
-    .catch()
     
   );
 });
